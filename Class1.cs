ceb984e733832a06e1b10109168add647eda064b
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-08 12:03:43 +0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace MagicTrick
+{
+    public class MagicTrick
+    {
+        [UnmanagedCallersOnly(EntryPoint = "gmod13_open")]
+        public static int gmod13_open(IntPtr lua_state)
+        {
+            File.WriteAllText("./MagicTrick.txt", "IT WORKS!!!");
+            return 0;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "gmod13_close")]
+        public static int gmod13_close(IntPtr lua_state)
+        {
+            return 0;
+        }
+    }
+}
